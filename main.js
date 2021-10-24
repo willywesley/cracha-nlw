c69eccab04b33c902b5447d37aba406696db7436
Willy Wesley <63491895+willywesley@users.noreply.github.com>
2021-10-24 13:03:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+const linksSocialMedia = {
+  github: 'willywesley',
+  youtube: 'willywesley',
+  instagram: 'willwesley',
+  facebook: 'maykbrito',
+  twitter: 'willywesley'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+  }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(Response => Response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userPhotoProfileLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
